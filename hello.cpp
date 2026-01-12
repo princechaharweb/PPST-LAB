cf1be19aa30d5392be6b66020677fa241d353909
Prince Chahar <princechahar456@gmail.com>
2026-01-12 14:24:12 +0530

    This is my first cpp program

---
+++ hello.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+using namespace std ;
+int main()
+{
+     float a,r,l,b,x ;
+    // cout << "enter radius of circle : " ;
+    // cin >> r ;
+    // a = 3.14*r*r ;
+    // cout << "area of circle : " << a << endl ;
+    // return 0;
+
+    cout << "enter length and breadth of rectangle : " ;
+    cin >> l >> b ;
+    x = l*b ;
+    cout << "area of rectangle : " << x << endl ;
+    return 0;
+
+}
+
