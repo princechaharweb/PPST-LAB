23efe15b30fd47ce0d6d78ab0c97714266a6310b
Prince Chahar <princechahar456@gmail.com>
2026-01-12 14:29:31 +0530

    added some more areas

--- hello.cpp
+++ hello.cpp
@@ -2,17 +2,28 @@
 using namespace std ;
 int main()
 {
-     float a,r,l,b,x ;
+     float a,r,l,b,x,y,z,h,i,p;
     // cout << "enter radius of circle : " ;
     // cin >> r ;
     // a = 3.14*r*r ;
     // cout << "area of circle : " << a << endl ;
     // return 0;
 
-    cout << "enter length and breadth of rectangle : " ;
-    cin >> l >> b ;
-    x = l*b ;
-    cout << "area of rectangle : " << x << endl ;
+    // cout << "enter length and breadth of rectangle : " ;
+    // cin >> l >> b ;
+    // x = l*b ;
+    // cout << "area of rectangle : " << x << endl ;
+    // return 0;
+
+    // cout << "enter side of square : " ;
+    // cin >> y ;
+    // z = y*y ;
+    // cout << "area of square : " << z << endl ;
+
+    cout << "enter height and base of a triangle : " ;
+    cin >> h >> i ;
+    p = 0.5*h*i ;
+    cout << "area of triangle : " << p << endl ;
     return 0;
 
 }
