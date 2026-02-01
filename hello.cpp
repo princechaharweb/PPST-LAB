92cf968bd85b9ffdd9e5182b6454f138f95e08d9
Prince Chahar <princechahar456@gmail.com>
2026-02-01 17:27:23 +0530

    compound interest

--- hello.cpp
+++ hello.cpp
@@ -36,40 +36,62 @@
 //             cout << "Invalid operator";
 //     }
 //     return 0;}
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+// float side, radius, height, length, breadth;
+//     float volumeCube, volumeCylinder;
+//     float areaTriangle, areaRectangle;
+
+//      cout << "Enter side of cube: ";
+//     cin >> side;
+
+//     volumeCube = side * side * side;
+//     cout << "Volume of Cube = " << volumeCube << endl;
+
+//      cout << "Enter radius of cylinder: ";
+//     cin >> radius;
+//     cout << "Enter height of cylinder: ";
+//     cin >> height;
+
+//     volumeCylinder = 3.14 * radius * radius * height;
+//     cout << "Volume of Cylinder = " << volumeCylinder << endl;
+
+//      cout << "Enter side of equilateral triangle: ";
+//     cin >> side;
+//     areaTriangle = (1.73 * side * side) / 4;
+
+//     cout << "Area of Equilateral Triangle = " << areaTriangle << endl;
+
+//      cout << "Enter length of rectangle: ";
+//     cin >> length;
+//     cout << "Enter breadth of rectangle: ";
+//     cin >> breadth;
+//     areaRectangle = length * breadth;
+//     cout << "Area of Rectangle = " << areaRectangle << endl;
+//     return 0;
+// }
+
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
-float side, radius, height, length, breadth;
-    float volumeCube, volumeCylinder;
-    float areaTriangle, areaRectangle;
+int main(){
+double power;
+    double ci,a,p,r,n,t;
+    cout<<"Enter the principle value "<<endl;
+    cin>>p;
+    cout<<"Enter the rate "<< endl;
+    cin>>r;
+    cout<<"Enter the value of n "<<endl;
+    cin>>n;
+    cout<<"Enter the time period "<<endl;
+    cin>>t;
+    a = (1+r/(100*n));
+    ci = p*pow(a,n*t);
+    cout<<"Value of compound interest is "<< ci<<endl;
 
-     cout << "Enter side of cube: ";
-    cin >> side;
-
-    volumeCube = side * side * side;
-    cout << "Volume of Cube = " << volumeCube << endl;
-
-     cout << "Enter radius of cylinder: ";
-    cin >> radius;
-    cout << "Enter height of cylinder: ";
-    cin >> height;
-
-    volumeCylinder = 3.14 * radius * radius * height;
-    cout << "Volume of Cylinder = " << volumeCylinder << endl;
-
-     cout << "Enter side of equilateral triangle: ";
-    cin >> side;
-    areaTriangle = (1.73 * side * side) / 4;
-
-    cout << "Area of Equilateral Triangle = " << areaTriangle << endl;
-
-     cout << "Enter length of rectangle: ";
-    cin >> length;
-    cout << "Enter breadth of rectangle: ";
-    cin >> breadth;
-    areaRectangle = length * breadth;
-    cout << "Area of Rectangle = " << areaRectangle << endl;
     return 0;
 }
 
