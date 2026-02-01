48774f6a5c6c1edf721c88d3978192406d1039b3
Prince Chahar <princechahar456@gmail.com>
2026-02-01 17:44:10 +0530

    grade system

--- hello.cpp
+++ hello.cpp
@@ -182,17 +182,25 @@
 using namespace std;
 
 int main() {
-    int a, b, c;
-    cout << "Enter three numbers: ";
-    cin >> a >> b >> c;
-    if (a >= b && a >= c)
-        cout << "Maximum number is: " << a;
-    else if (b >= a && b >= c)
-        cout << "Maximum number is: " << b;
+    int marks;
+    cout << "Enter marks (0-100): ";
+    cin >> marks;
+
+    if (marks >= 90)
+        cout << "Grade: A";
+    else if (marks >= 75)
+        cout << "Grade: B";
+    else if (marks >= 60)
+        cout << "Grade: C";
+    else if (marks >= 40)
+        cout << "Grade: D";
     else
-        cout << "Maximum number is: " << c;
+        cout << "Grade: F (Fail)";
+
     return 0;
 }
+
+
 
 
     
