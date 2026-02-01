b9e36c3a2a0253f221b34ec9ebf9bd1290514063
Prince Chahar <princechahar456@gmail.com>
2026-02-01 17:16:55 +0530

    calculator

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <cmath> 
-#include <iomanip> 
+//#include <cmath> 
+//#include <iomanip> 
 
 using namespace std;
 
@@ -8,30 +8,48 @@ int main() {
    // double principal, rate, time, amount, compoundInterest;
 
     
-    cout << "Enter the principal amount: ";
-    cin >> principal;
+   //// cout << "Enter the principal amount: ";
+    //cin >> principal;
 
-    cout << "Enter the annual interest rate (in percentage): ";
-    cin >> rate;
+    //cout << "Enter the annual interest rate (in percentage): ";
+    //cin >> rate;
 
-    cout << "Enter the time period (in years): ";
-    cin >> time;
+   // cout << "Enter the time period (in years): ";
+    //cin >> time;
 
 
-    rate = rate / 100;
-
-    
-    amount = principal * pow((1 + rate), time);
+    //rate = rate / 100;
 
     
-    compoundInterest = amount - principal;
+    //amount = principal * pow((1 + rate), time);
 
     
-    cout << fixed << setprecision(2);
-    cout << "\nTotal Amount after " << time << " years: " << amount << endl;
-    cout << "Compound Interest: " << compoundInterest << endl;
+    //compoundInterest = amount - principal;
 
-    return 0;
+    
+    //cout << fixed << setprecision(2);
+    //cout << "\nTotal Amount after " << time << " years: " << amount << endl;
+    //cout << "Compound Interest: " << compoundInterest << endl;
+
+//     int a, b;
+//       cout << "Enter two integers: ";
+//       cin >> a >> b;
+//       if (a > b)
+//       cout << a << " is the largest number." << endl;
+//       else(b > a);
+//       cout << b << " is the largest number." << endl;
+//       return 0;
+      int a,b,c,d;
+      cout<<"Enter three numbers:";
+      cin>>a>>b>>c;
+      if(a>=b)
+      cout<<a<<" is the largest number."<<endl;
+      else if(b>=c)           
+      cout<<b<<" is the largest number."<<endl;
+      else        
+
+      cout<<c<<" is the largest number."<<endl;
+      return 0;   
 }
 
     
